21c1f961a3b86db1511faa64b85425f1f1cc7a93
Carver Hannasch <awizard1011@gmail.com>
2024-11-06 19:52:05 -0500

    add mobile layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,58 +12,131 @@ export default function Header(props) {
 
   return (
     <header className="header page__section ">
-      <div className="header__content-wrapper">
-        <Link to="/" className="header__link">
-          <img className="header__logo" src={headerlogo} alt="wtwr" />
-        </Link>
-        <p className="header__text header__date">{`${currentDate}, ${props.weatherInfo.name}`}</p>
-      </div>
-      <div className="header__content-wrapper">
-        <ToggleSwitch />
-        {props.isLoggedIn ? (
-          <>
-            <button
-              className="header__text-button header__add-clothes"
-              type="button"
-              onClick={() => {
-                props.setOpenedModal("add-clothes-form");
-              }}
-            >
-              + Add clothes
-            </button>
-            <Link to="/profile" className="header__link">
-              <p className="header__text header__name">{currentUser.name}</p>
-              {}
-              <img
-                className="header__profile-picture"
-                src={currentUser.avatar}
-                alt={currentUser.name}
-              />
+      {window.innerWidth > 450 ? (
+        <>
+          <div className="header__content-wrapper">
+            <Link to="/" className="header__link">
+              <img className="header__logo" src={headerlogo} alt="wtwr" />
             </Link>
-          </>
-        ) : (
-          <>
-            <button
-              className="header__text-button header__sign-up"
-              type="button"
-              onClick={() => {
-                props.setOpenedModal("sign-up-form");
-              }}
-            >
-              Sign Up
-            </button>
-            <button
-              className="header__text-button header__log-in"
-              type="button"
-              onClick={() => {
-                props.setOpenedModal("log-in-form");
-              }}
-            >
-              Log In
-            </button>
-          </>
-        )}
-      </div>
+            <p className="header__text header__date">{`${currentDate}, ${props.weatherInfo.name}`}</p>
+          </div>
+          <div className="header__content-wrapper">
+            <ToggleSwitch />
+            {props.isLoggedIn ? (
+              <>
+                {window.innerWidth > 1050 ? (
+                  <button
+                    className="header__text-button header__add-clothes"
+                    type="button"
+                    onClick={() => {
+                      props.setOpenedModal("add-clothes-form");
+                    }}
+                  >
+                    + Add clothes
+                  </button>
+                ) : (
+                  ""
+                )}
+
+                <Link to="/profile" className="header__link">
+                  {window.innerWidth > 800 && (
+                    <p className="header__text header__name">
+                      {currentUser.name}
+                    </p>
+                  )}
+
+                  <img
+                    className="header__profile-picture"
+                    src={currentUser.avatar}
+                    alt={currentUser.name}
+                  />
+                </Link>
+              </>
+            ) : (
+              <>
+                <button
+                  className="header__text-button header__sign-up"
+                  type="button"
+                  onClick={() => {
+                    props.setOpenedModal("sign-up-form");
+                  }}
+                >
+                  Sign Up
+                </button>
+                <button
+                  className="header__text-button header__log-in"
+                  type="button"
+                  onClick={() => {
+                    props.setOpenedModal("log-in-form");
+                  }}
+                >
+                  Log In
+                </button>
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <div className="header__mobile-layout">
+          <Link to="/" className="header__link">
+            <img className="header__logo" src={headerlogo} alt="wtwr" />
+          </Link>
+          <p className="header__text header__date">{`${currentDate}, ${props.weatherInfo.name}`}</p>
+          {props.isLoggedIn ? (
+            <>
+              {window.innerWidth > 1050 ? (
+                <button
+                  className="header__text-button header__add-clothes"
+                  type="button"
+                  onClick={() => {
+                    props.setOpenedModal("add-clothes-form");
+                  }}
+                >
+                  + Add clothes
+                </button>
+              ) : (
+                ""
+              )}
+
+              <Link to="/profile" className="header__link">
+                {window.innerWidth > 800 && (
+                  <p className="header__text header__name">
+                    {currentUser.name}
+                  </p>
+                )}
+
+                <img
+                  className="header__profile-picture"
+                  src={currentUser.avatar}
+                  alt={currentUser.name}
+                />
+              </Link>
+            </>
+          ) : (
+            <>
+              <button
+                className="header__text-button header__sign-up"
+                type="button"
+                onClick={() => {
+                  props.setOpenedModal("sign-up-form");
+                }}
+              >
+                Sign Up
+              </button>
+              <button
+                className="header__text-button header__log-in"
+                type="button"
+                onClick={() => {
+                  props.setOpenedModal("log-in-form");
+                }}
+              >
+                Log In
+              </button>
+            </>
+          )}
+          <ToggleSwitch />
+        </div>
+      )}
     </header>
   );
 }
